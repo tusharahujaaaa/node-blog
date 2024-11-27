66433976b7851e793b33cb0f674e734bb4a5ebc0
Tushar-NP <123053788+Tushar-NP@users.noreply.github.com>
2024-11-27 17:15:28 +0530

    first commit

---
+++ route/posts.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+// INDEX: Show all posts
+router.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", { posts });
+});
+
+// NEW: Show form to create new post
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+
+// CREATE: Add new post to database
+router.post("/", async (req, res) => {
+  await Post.create(req.body.post);
+  res.redirect("/posts");
+});
+
+// SHOW: Show details of one post
+router.get("/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("show", { post });
+});
+
+// EDIT: Show edit form for one post
+router.get("/:id/edit", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("edit", { post });
+});
+
+// UPDATE: Update a particular post
+router.put("/:id", async (req, res) => {
+  await Post.findByIdAndUpdate(req.params.id, req.body.post);
+  res.redirect(`/posts/${req.params.id}`);
+});
+
+
+// DELETE: Delete a particular post
+router.delete("/:id", async (req, res) => {
+  await Post.findByIdAndDelete(req.params.id);
+  res.redirect("/posts");
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const app = express();
+
+// Middleware
+app.set("view engine", "ejs"); // Use EJS
+app.use(express.static("public")); // Serve static files
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb+srv://crud:Crud@cluster0.mxla6.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Connection error:", err));
+
+// Routes
+const postRoutes = require("./route/posts");
+app.use("/posts", postRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
